6325543663c62ddffd991b49c1dedd570505bad4
GehaniK24 <gehanik24@greenhill.org>
2020-12-18 18:30:14 -0600

    Updating

--- src/RadmeClass.java
+++ src/RadmeClass.java
@@ -1,17 +1,17 @@
-import java.io.*;
-import java.util.*;
-public class RadmeClass {
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner input = new Scanner(new File("C:\\Users\\GehaniK24\\IdeaProjects\\Java 2020_2021\\src\\readme.txt"));
-        int count = 0;
-        while (input.hasNextLine()) {
-            String line = input.nextLine();
-            Scanner mini = new Scanner(line);
-            count++;
-            int wordCount = 0
-                    while (mini.hasNext());
-                    stocks[count] count++;
-        }
-        System.out.println(count + " total");
-    }
-}
+//import java.io.*;
+//import java.util.*;
+//public class RadmeClass {
+//    public static void main(String[] args) throws FileNotFoundException {
+//        Scanner input = new Scanner(new File("C:\\Users\\GehaniK24\\IdeaProjects\\Java 2020_2021\\src\\readme.txt"));
+//        int count = 0;
+//        while (input.hasNextLine()) {
+//            String line = input.nextLine();
+//            Scanner mini = new Scanner(line);
+//            count++;
+//            int wordCount = 0
+//                    while (mini.hasNext());
+//                    stocks[count] count++;
+//        }
+//        System.out.println(count + " total");
+//    }
+//}
