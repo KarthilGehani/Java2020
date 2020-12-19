1f982ee5674d0100352df4cb473c87099680bfb9
GehaniK24 <gehanik24@greenhill.org>
2020-12-18 18:21:37 -0600

    Updating

--- src/Stocks.java
+++ src/Stocks.java
@@ -1,58 +1,62 @@
-import java.util.Date;
-
+//This program is for monitoring your stock account
+import java.time.LocalDate;
+import java.util.Scanner;
 public class Stocks {
 //name of stock
     private String Name;
 //Price of stock when bought
     private double Pricewhenpurchased;
 //date of purchase
-    private Date PurchaseDate;
+    private String PurchaseDate;
 //amount of stock purchased
     private int QuantityofStock;
 //    The price of your stock now
     private double CurrentPrice;
 //    Did they buy(True) or sell (sell)
     private boolean Buyorsell;
-//    the default code
+//    the account number
+    private int AccountNumber;
+//    default
     public Stocks(){
         Name = "empty";
         QuantityofStock = 0;
     }
-
-    public Stocks(String name1, double purchaseprice1, Date purchasedate1, int quantity1, double currentprice1, boolean buyorsell1) {
+//    This is to make the set and get
+    public Stocks(String name1, double purchaseprice1, String purchasedate1, int quantity1, double currentprice1, boolean buyorsell1, int accountnumber1) {
         Name = name1;
         Pricewhenpurchased = purchaseprice1;
         PurchaseDate = purchasedate1;
         QuantityofStock = quantity1;
         CurrentPrice = currentprice1;
         Buyorsell = buyorsell1;
+        AccountNumber = accountnumber1;
     }
-//    These are allof my set variables
+//    These are all of my set variables
     public void setName(String name1){
        Name = name1;
     }
     public void setPricewhenpurchased(double purchaseprice1){
         Pricewhenpurchased = purchaseprice1;
     }
-    public void setPurchaseDate(Date purchasedate1){
+    public void setPurchaseDate(String purchasedate1){
         PurchaseDate = purchasedate1;
     }
     public void setQuantityofStock(int quantity1){
         QuantityofStock = quantity1;
     }
-    public void setCurrentPrice(double currentprice1){
-        CurrentPrice = currentprice1;
-    }
+    public void setCurrentPrice(double currentprice1){ CurrentPrice = currentprice1; }
     public void setBuyorsell(boolean buyorsell1){
         Buyorsell = buyorsell1;
     }
+    public void SetAccountNumber(int SetAccountNumber) {AccountNumber = SetAccountNumber;}
+//    These are all of my get variables
     public String getName(){
         return Name;
     }
     public double getPricewhenpurchased(){
         return Pricewhenpurchased;
     }
-    public Date getPurchaseDate(){
+    public String getPurchaseDate(){
         return PurchaseDate;
     }
     public int getQuantityofStock(){
@@ -61,13 +65,16 @@ public class Stocks {
     public double getCurrentPrice(){
         return CurrentPrice;
     }
-    public boolean setBuyorsell(){
+    public boolean getBuyorsell(){
         return Buyorsell;
     }
+    public int getAccountNumber() {return AccountNumber;}
+//    This is a method to calculate profit
     public boolean Profit(){
         double Profit = (CurrentPrice - Pricewhenpurchased) * QuantityofStock;
         return Profit();
     }
+//    This is my tostring for printing
     public String toString() {
         return "The name of your stock is "+Name+" the date you purchased it on is "+PurchaseDate+" the price you purchased the stock for was "+Pricewhenpurchased+" the amount of this stock you purchased is "+QuantityofStock+" the current price of the stock is "+CurrentPrice+" Did you buy or sell the stock "+Buyorsell;
     }
--- src/StocksClient.java
+++ src/StocksClient.java
@@ -1,59 +1,115 @@
 import java.io.File;
+import java.time.LocalDate;
+import java.util.*;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 public class StocksClient {
-    public static void main(String[] args) {
-        public static void Menu () {
-            System.out.println("                  Stock Management System                        ");
-            System.out.println("        1. What is the name of your stock?                       ");
-            System.out.println("        2. WHen did you purchase this stock?                     ");
-            System.out.println("        3. How much did your stock cost when you purchased it?   ");
-            System.out.println("        4. How much of this stock did you purchase?              ");
-            System.out.println("        5. What was the price of the stock when you purchased it?");
-            System.out.println("        6. Do you want to buy or sell you stock?                 ");
-            System.out.println("        7. Exit                                                  ");
-            Scanner input = new Scanner(System.in);
-            int beloworabove = input.nextInt();
-            switch (beloworabove) {
-                case 1:
+    LocalDate mydate = LocalDate.now();
+//    This is for the amount of stocks in my databse
+    public static Stocks[] stock = new Stocks[20];
+    public static void main(String[] args) throws FileNotFoundException {
+//       This is to copy path from the file
+        Scanner fileinput = new Scanner (new File("C:\\Users\\GehaniK24\\IdeaProjects\\Java 2020_2021\\src\\FinalProdjectFile"));
+            while (fileinput.hasNextLine()) {
+                String line = fileinput.nextLine();
+                Scanner mini = new Scanner(line);
+                while (mini.hasNext()) {
+                    String Name = mini.next();
+                    double Pricewhenpurchased = mini.nextDouble();
+                    String PurchaseDate = mini.next();
+                    int QuantityofStock = mini.nextInt();
+                    double CurrentPrice = mini.nextDouble();
+                    boolean Buyorsell = mini.nextBoolean();
+                    int AccountNumber = mini.nextInt();
+                }
+            }
+//            this is my menu and the questions i ask
+                System.out.println("                  Stock Management System                        ");
+                System.out.println("        1. What is your account number?                           ");
+                System.out.println("        2. Do you want to buy stock?                            ");
+                System.out.println("        3. Do you want to sell stock?   ");
+                System.out.println("        4. Do you want to close a position?              ");
+                System.out.println("        5. Do yo want to know your acount balance?");
+                System.out.println("        6. Exit                                                  ");
+                Scanner input = new Scanner(System.in);
+                int beloworabove = input.nextInt();
+//               these are my switch and cases so I can call my methods based on the number of each question
+                switch (beloworabove) {
+                    case 1:
+                        AccountNumber(input);
+                        break;
+                    case 2:
+                        BuyStock(input);
+                        break;
+                    case 3:
+                        SellStock(input);
+                        break;
+                    case 4:
+                        ClosePosition(input);
+                        break;
+                    case 5:
+                        AccountBalance(input);
+                        break;
+                    case 6:
+                        exit(input);
+                        break;
 
-                    break;
-                case 2:
+                }
 
-                    break;
-                case 3:
 
-                    break;
-                case 4:
-
-                    break;
-                case 5:
-
-                    break;
-                case 6:
-
-                    break;
-                case 7:
-
-                    break;
             }
 
-
+//This is my method for the first question to find the account number
+    public static void AccountNumber(Scanner input){
+        System.out.println("What is your account number?");
+        int AccountNumber = input.nextInt();
         }
-        if (BuyorSell)
-
-            Scanner input = new Scanner(new File("C:\\Users\\GehaniK24\\IdeaProjects\\Java 2020_2021\\src\\readme.txt"));
-        int count = 0;
-        while (input.hasNextLine()) {
-            String line = input.nextLine();
-            Scanner mini = new Scanner(line);
-            count++;
-            int wordCount = 0
-            while (mini.hasNext()) ;
-            int
-        }
-        System.out.println(count + " total");
+//        This is my method for the second question to see if I want to buy stock
+    public static void BuyStock(Scanner input){
+        System.out.println("Do you want to buy stock?");
+        Boolean BuyStock = input.nextBoolean();
+            System.out.println("Which stock would you like to buy?");
+            String name = input.next();
+            System.out.println("What quantity of stock would you like to buy?");
+            int quantity = input.nextInt();
+            System.out.println("What is the price of the stock you want to buy?");
+            double price = input.nextDouble();
+            }
+//            This is my method to see if you want to sell any of your existing stocks
+    public static void SellStock(Scanner input){
+        System.out.println("Do you want to sell stock?");
+        int SellStock = input.nextInt();
+                System.out.println("Which stock would you like to sell?");
+                String name = input.next();
+                System.out.println("How much of that stock would you like to sell?");
+                int quantity = input.nextInt();
+                System.out.println("What is the current price of the stock you want to sell?");
+                double price = input.nextDouble();
+    }
+//    This is the method to see if you want to close a position and if you want to buy or sell
+    public static void ClosePosition(Scanner input){
+        System.out.println("Do you want to close a position");
+        String closeposition = input.next();
+        System.out.println("Which stock do you want to close?");
+        String name = input.next();
+        System.out.println("What is the current price of the stock you want to close?");
+        double price = input.nextDouble();
+        System.out.println("Do you wantto buy or sell?");
+        String buyorsell  = input.next();
+        System.out.println("How much of that stock would you like to close?");
+        int closequantity = input.nextInt();
+    }
+//    This is my method to calculate to account balance
+    public static void AccountBalance(Scanner input){
+        System.out.println("Do you want to know your account balance?");
+        double accountbalance = input.nextDouble();
+    }
+//    Thia ia my method to exit the program
+    public static void exit(Scanner input){
+        System.out.println("Do you want to exit?");
+        String exit = input.next();
+    }
 
 
     }
-}
+
+
